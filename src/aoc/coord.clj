65bf47fac565ce7a70efe29465a4dfaba6792859
SimY4 <alex.simkin@outlook.com>
2025-01-09 19:05:05 +1100

    2024 day 8.

--- src/aoc/coord.clj
+++ src/aoc/coord.clj
@@ -4,23 +4,20 @@
 (def directions (concat hv-directions [[1 1] [1 -1] [-1 1] [-1 -1]]))
 
 (defn + [& more] 
-  (reduce 
-    (fn [[x y] [x' y']] [(clojure.core/+ x x') (clojure.core/+ y y')])
-    [0 0]
-    more))
+  (reduce #(mapv clojure.core/+ %1 %2) [0 0] more))
 
-(defn opposite [[x y]] [(- x) (- y)])
+(defn opposite [coord] (mapv - coord))
 
 (defn left [coord]
-  (cond 
-    (= [0 1] coord) [-1 0]
-    (= [0 -1] coord) [1 0]
-    (= [1 0] coord) [0 1]
-    (= [-1 0] coord) [0 -1]
-    (= [1 1] coord) [-1 1]
-    (= [1 -1] coord) [1 1]
-    (= [-1 1] coord) [-1 -1]
-    (= [-1 -1] coord) [1 -1]))
+  (case coord 
+    [0 1] [-1 0]
+    [0 -1] [1 0]
+    [1 0] [0 1]
+    [-1 0] [0 -1]
+    [1 1] [-1 1]
+    [1 -1] [1 1]
+    [-1 1] [-1 -1]
+    [-1 -1] [1 -1]))
 
 (defn right [coord] (opposite (left coord)))
 
